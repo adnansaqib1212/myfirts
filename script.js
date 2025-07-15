8ae6a4b26e09d18fd8d2839e8d9c504985b7b058
adnansaqib1212 <adnsaq@gmail.com>
2025-07-15 18:50:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// VanillaTilt automatically initializes from the HTML attribute data-tilt
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.2,
+  });
+  
